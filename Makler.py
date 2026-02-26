da79f3f53172da9160ac2540e6209cbb8729550c
Mirko Sturm <msturm@mpipz.mpg.de>
2026-02-26 10:46:00 +0100

    Rechnung_Raum_kleiner

--- Makler.py
+++ Makler.py
@@ -7,14 +7,24 @@ def Rechnung(seite_a, seite_b):
 
     return fläche
 
-wert_a = int(input("Wert 1: "))
-wert_b = int(input("Wert 2: "))
 
+raum = input("Bezeichnung Raum: ")
+gwert_a = int(input("Wert 1: "))
+gwert_b = int(input("Wert 2: "))
+
+fläche_raum = Rechnung(gwert_a, gwert_b)
 print(Rechnung(gwert_a, gwert_b))
 
+tuple_raum = (raum, fläche_raum)
+print(tuple_raum)
 print("Ist der Raum ein rechteck?(J)a / (N)ein")
 
-if input() == "j":
+antwort = input(" j oder n")
+if antwort == "j":
     print("TOP")
-elif input() == "n":
+elif antwort == "n":
     print("NEIN")
+    gwert_c = int(input("Wert 1: "))
+    gwert_d = int(input("Wert 2: "))
+    fläche_raum = fläche_raum - Rechnung(gwert_c, gwert_d)
+    print(fläche_raum)
